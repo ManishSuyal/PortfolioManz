e91642c524e778c95005b305ef292e5f63af542c
Manish Suyal <manishsuyal2099@Manishs-MacBook-Air.local>
2024-02-22 22:54:36 +0530

    final touch done and issues fix

--- src/data/data.js
+++ src/data/data.js
@@ -1,35 +1,30 @@
 // import WorkIm from '../assets/projects/workImg.jpeg'
 // import RealEst from '../assets/projects/realestate.jpg'
+import ev  from "../assets/ev.png"
+import shopper  from "../assets/shopper.png"
+import notes  from "../assets/notes.png"
 
 export const data=[
     {
         id:1,
-        name:"React JS Application",
-        image:"none",
-        github:"",
-        live:"",
+        name:"Ev zone",
+        image: ev,
+        github:"https://github.com/ManishSuyal/Electric_bikes",
+        live:"https://manishsuyal.github.io/Electric_bikes/",
     },
     {
         id:2,
-        name:"React JS Application",
-        image:"none",
-        github:"",
-        live:"",
+        name:"Ecommerce UI",
+        image: shopper,
+        github:"https://github.com/ManishSuyal/Ecommerce-shop",
+        live:"https://statuesque-otter-712c0d.netlify.app/",
     },
     {
         id:3,
-        name:"React JS Application",
-        image:"none",
-        github:"",
-        live:"",
-    },
-    {
-        id:4,
-        name:"React JS Application",
-        image:"none",
-        github:"",
-        live:"",
-    },
-
+        name:"Note It down",
+        image: notes,
+        github:"https://github.com/ManishSuyal/NoteItDown",
+        live:"https://note-it-down1000.netlify.app/",
+    }
 
 ]
