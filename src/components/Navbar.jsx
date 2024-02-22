c86259ec6e42de89c3bc2f6718d4d7f66cf5831d
Manish Suyal <manishsuyal2099@Manishs-MacBook-Air.local>
2024-02-22 17:28:44 +0530

    contact page added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,62 @@
-import React, { useState } from 'react'
-import Logo from "../assets/logo.png"
-import { FaBars , FaTimes } from 'react-icons/fa'
-const Navbar = () => {
-  const [nav , setNav] = useState(false)
-  const handleClick = () => setNav(!nav)
-  return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-50'>
-        <div>
-          <img src={Logo} alt="Image Logo" style={{width:'50px'}} />
-        </div>
-          <ul className='hidden md:flex'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Skills</li>
-            <li>Contact</li>
-          </ul>
-        {/* hamburger menu */}
-        <div onClick={handleClick} className='md:hidden z-10'>
-         {!nav ? <FaBars /> : <FaTimes/>}
-        </div>
-        {/* Mobile menu */}
-        
-        <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-            <li className='py-6 text-4xl'>Home</li>
-            <li className='py-6 text-4xl'>About</li>
-            <li className='py-6 text-4xl'>Skills</li>
-            <li className='py-6 text-4xl'>Contact</li>
-          </ul>
-          {/* Social Icons  */}
-          <div className='hidden'></div>
-    </div>
-  )
-}
+import React, { useState } from "react";
+import ms from "../assets/ms.png";
+import { Link } from "react-scroll";
 
-export default Navbar
+import { FaBars, FaTimes } from "react-icons/fa";
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-50">
+      <div>
+        <img src={ms} alt="Image Logo" style={{ width: "110px" }} />
+      </div>
+      <ul className="hidden md:flex">
+        <li>
+      
+          <Link to="Home" smooth={true} duration={500}>
+
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="About" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="Skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li>
+          <Link to="Contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+      {/* hamburger menu */}
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+      {/* Mobile menu */}
+
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl"><Link onClick={handleClick} to="Home" smooth={true} duration={500}>Home</Link></li>
+        <li className="py-6 text-4xl"><Link onClick={handleClick} to="About" smooth={true} duration={500}>About</Link></li>
+        <li className="py-6 text-4xl"><Link onClick={handleClick} to="Skills" smooth={true} duration={500}>Skills</Link></li>
+        <li className="py-6 text-4xl"><Link onClick={handleClick} to="Contact" smooth={true} duration={500}>Contact</Link></li>
+      </ul>
+      {/* Social Icons  */}
+      <div className="hidden"></div>
+    </div>
+  );
+};
+
+export default Navbar;
