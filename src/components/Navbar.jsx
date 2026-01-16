c20cd01a18f232ce1d6c191753a67f871d92f57d
Manish Suyal <manishsuyal2099@Manishs-Air.alphionsee.in>
2026-01-16 23:51:28 +0530

    updates jan26

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -108,7 +108,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a target="_blank"
               className='flex justify-between items-center w-full text-gray-300'
-              href='https://drive.google.com/file/d/1IpYqgPfvWl3AhLekdlD7m6JdhwQF_lBq/view'
+              href='https://drive.google.com/file/d/1I4u-1_xrc5VrEy0b6uAz1FRMF87h8tzw/view?usp=sharing'
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
