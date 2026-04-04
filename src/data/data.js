8d56d2e8ef8d8ec7a09a6b07bd7a49640aceafb8
Manish Suyal <manishsuyal2099@Manishs-Air.alphionsee.in>
2026-04-05 01:34:19 +0530

    updates april

--- src/data/data.js
+++ src/data/data.js
@@ -7,24 +7,32 @@ import notes  from "../assets/notes.png"
 export const data=[
     {
         id:1,
-        name:"Ev zone",
+        name:"Ev zone MVP",
         image: ev,
         github:"https://github.com/ManishSuyal/Electric_bikes",
         live:"https://manishsuyal.github.io/Electric_bikes/",
     },
     {
         id:2,
-        name:"Ecommerce UI",
+        name:"Ecommerce MVP",
         image: shopper,
         github:"https://github.com/ManishSuyal/Ecommerce-shop",
         live:"https://statuesque-otter-712c0d.netlify.app/",
     },
     {
         id:3,
-        name:"Note It down",
+        name:"Note It down MVP",
         image: notes,
         github:"https://github.com/ManishSuyal/NoteItDown",
         live:"https://note-it-down1000.netlify.app/",
+    }
+    ,
+    {
+        id:4,
+        name:"Amazon sales dashboard",
+        image: notes,
+        github:"text",
+        live:"https://public.tableau.com/views/AmazonSalesDashboard_17705397272710/WelcomePage?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
     }
 
 ]
