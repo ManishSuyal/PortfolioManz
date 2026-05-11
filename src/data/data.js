d3c9b49ef9b6a7ae89fca169eb100b4366221ed2
Manish Suyal <manishsuyal2099@Manishs-Air.alphionsee.in>
2026-05-11 22:25:26 +0530

    updates may projects

--- src/data/data.js
+++ src/data/data.js
@@ -4,24 +4,39 @@ import ev  from "../assets/ev.png"
 import shopper  from "../assets/shopper.png"
 import notes  from "../assets/notes.png"
 import Dashboard from "../assets/Dashboard.png"
+import AE from "../assets/AE.png"
+import AEP from "../assets/AEP.png"
+
 
 export const data=[
     {
         id:1,
+        name:"American Express Product Teardown",
+        image: AE,
+        live:"https://drive.google.com/file/d/1Y7rZk0h75K1zBMHYOqFM76MvBq8AP-QJ/view?usp=sharing",
+    },
+    {
+        id:2,
+        name:"PRD : Expanding Amex Tier 2/3 cities Accessibility",
+        image: AEP,
+        live:"https://drive.google.com/file/d/12bVuBplqINlRQWm0V3zRb6w1W_zpLnRw/view?usp=sharing",
+    },
+    {
+        id:3,
         name:"Ev zone MVP",
         image: ev,
         github:"https://github.com/ManishSuyal/Electric_bikes",
         live:"https://manishsuyal.github.io/Electric_bikes/",
     },
     {
-        id:2,
+        id:4,
         name:"Ecommerce MVP",
         image: shopper,
         github:"https://github.com/ManishSuyal/Ecommerce-shop",
         live:"https://statuesque-otter-712c0d.netlify.app/",
     },
     {
-        id:3,
+        id:5,
         name:"Note It down MVP",
         image: notes,
         github:"https://github.com/ManishSuyal/NoteItDown",
@@ -29,7 +44,7 @@ export const data=[
     }
     ,
     {
-        id:4,
+        id:6,
         name:"Amazon sales dashboard",
         image: Dashboard,
         live:"https://public.tableau.com/views/AmazonSalesDashboard_17705397272710/WelcomePage?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
